0b2c4319dd8db5f4025a629e4b28fc5dc05e0921
Patosya <p4t0sya@gmail.com>
2024-11-04 02:44:05 +0300

    Added main function

---
+++ src/server/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"Golang-Proxy-Youtube/internal/server"
+	"log"
+	"os"
+)
+
+func main() {
+	port := "50051"
+	cache := server.NewMemoryCache()
+	if err := server.Start(port, cache); err != nil {
+		log.Fatalf("Ошибка запуска gRPC сервера: %v", err)
+		os.Exit(1)
+	}
+}
