370f8b9653af3ab51ef4df9428eed2135dd9f5fc
Patosya <p4t0sya@gmail.com>
2024-11-07 20:59:33 +0300

    Fixed redis

--- pkg/cache/redis.go
+++ pkg/cache/redis.go
@@ -2,24 +2,35 @@ package cache
 
 import (
 	"context"
+	"time"
 
 	"github.com/redis/go-redis/v9"
 )
 
-type RedisCache struct {
+type RedisCache struct{
 	client *redis.Client
-	ctx    context.Context
+	ctx context.Context
 }
-
-func NewRedisCache(addr, password string, db int) *RedisCche {
+func NewRedisCache(addr, password string, db int) *RedisCache{
 	ctx := context.Background()
 	client := redis.NewClient(&redis.Options{
-		Addr:     addr,
-		password: password,
-		DB:       db,
+		Addr: addr,
+		Password: password,
+		DB: db,
 	})
-	return &RedisClient{
+	return &RedisCache{
 		client: client,
-		ctx:    ctx,
+		ctx: ctx,
 	}
+}
+func (r *RedisCache) Set(key string, value[]byte, expiration time.Duration)error{
+	return r.client.Set(r.ctx, key, value, expiration).Err()
+}
+
+func (r *RedisCache) Get(key string)([]byte, error){
+	return r.client.Get(r.ctx, key).Bytes()
+}
+func (r *RedisCache) Close()error{
+	return r.client.Close()
+
 }
