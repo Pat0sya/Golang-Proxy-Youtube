916513cde51adc65275962f8232efe364b13359d
Patosya <p4t0sya@gmail.com>
2024-11-07 20:58:00 +0300

    Added redis implementation

--- go.mod
+++ go.mod
@@ -5,6 +5,9 @@ go 1.23.2
 require google.golang.org/grpc v1.67.1
 
 require (
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/redis/go-redis/v9 v9.7.0 // indirect
 	golang.org/x/net v0.28.0 // indirect
 	golang.org/x/sys v0.24.0 // indirect
 	golang.org/x/text v0.17.0 // indirect
--- pkg/cache/redis.go
+++ pkg/cache/redis.go
@@ -1,1 +1,25 @@
 package cache
+
+import (
+	"context"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type RedisCache struct {
+	client *redis.Client
+	ctx    context.Context
+}
+
+func NewRedisCache(addr, password string, db int) *RedisCche {
+	ctx := context.Background()
+	client := redis.NewClient(&redis.Options{
+		Addr:     addr,
+		password: password,
+		DB:       db,
+	})
+	return &RedisClient{
+		client: client,
+		ctx:    ctx,
+	}
+}
